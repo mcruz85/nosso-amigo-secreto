f25e075791ab48f69829b5f2dcc9979f185c39f8
Marcus Cruz <marcus.cruz85@gmail.com>
2018-01-24 00:58:44 -0300

    setup mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,6 +42,10 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => 'mailcatcher', :port => 1025 }
+
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
