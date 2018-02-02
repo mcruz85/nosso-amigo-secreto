f37b1ce86f875104c9f5ce9c798250153e8f333a
Marcus Cruz <marcus.cruz85@gmail.com>
2018-02-02 02:19:45 -0300

    fix campaign controller spec

--- spec/controllers/campaigns_controller_spec.rb
+++ spec/controllers/campaigns_controller_spec.rb
@@ -61,8 +61,8 @@ RSpec.describe CampaignsController, type: :controller do
 
     it "Create campaign with right attributes" do
       expect(Campaign.last.user).to eql(@current_user)
-      expect(Campaign.last.title).to eql(@campaign_attributes[:title])
-      expect(Campaign.last.description).to eql(@campaign_attributes[:description])
+      expect(Campaign.last.title).to eql("Nova Campanha")
+      expect(Campaign.last.description).to eql("Descreva sua campanha...")
       expect(Campaign.last.status).to eql('pending')
     end
 
